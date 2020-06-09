707f5513e8570944ef2d393158b7ddf26cffd108
Marutsky <63045288+Marutsky@users.noreply.github.com>
2020-06-09 20:13:33 +0700

    update_SelectPage

--- Scorelink.web/Controllers/SelectPageController.cs
+++ Scorelink.web/Controllers/SelectPageController.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Scorelink.BO.Helper;
+using Scorelink.MO.DataModel;
+using Scorelink.BO.Repositories;
 
 namespace Scorelink.web.Controllers
 {
     public class SelectPageController : Controller
     {
+
+        DocumentInfoRepo docInfoRepo = new DocumentInfoRepo();
+        DocumentDetailRepo docDetRepo = new DocumentDetailRepo();
         // GET: SelectPage
         public ActionResult Index()
         {
-            return View();
+
+            return View("SelectPage");
         }
 
         public ActionResult SelectPage(string id)
         {
             ViewBag.Id = id;
-            return View();
+            //var data = docDetRepo.Get(id);
+            //ViewBag.Id = data.DocId;
+            //ViewBag.DocDetId = data.DocDetId;
+            //ViewBag.DocPageNo = data.DocPageNo;
+            //ViewBag.PageFileName = data.PageFileName;
+            //ViewBag.PagePath = data.PagePath;
+
+            //var docInfo = docInfoRepo.Get(iDocId);
+            //ViewBag.PDFPath = docInfo.FilePath;
+            return View("SelectPage");
         }
+
     }
 }
