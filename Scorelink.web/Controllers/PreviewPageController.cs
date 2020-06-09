ecabddc14e218b11f4cb4b46b2c3e1f2ea345aef
Marutsky <63045288+Marutsky@users.noreply.github.com>
2020-06-09 10:26:16 +0700

    update controller

---
+++ Scorelink.web/Controllers/PreviewPageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scorelink.MO.DataModel;
+using System.IO;
+//using OfficeOpenXml;
+
+namespace Scorelink.web.Controllers
+{
+    public class PreviewPageController : Controller
+    {
+        // GET: PreviewPage
+
+        public ActionResult Index()
+        {
+            //string xValue = Request.Form["txtPage"];
+            //  var xfileName = txtPage;
+
+
+
+
+            //ViewBag.Message = string.Format("Hello {0}.\\nCurrent Date and Time: {1}", name, DateTime.Now.ToString());
+            //return View("PreViewPage");
+            //ResultModel empobj = new ResultModel();
+            string textboxValue = Request.Form["txtPage"];
+            if (textboxValue != null)
+            {
+                //empobj.ScanEdit = CheckResult();
+                ViewData["Message"] = "select";
+            }
+            else
+            {
+                ViewData["Message"] = "notselect";
+                // empobj.ScanEdit = MergeRow();
+            }
+            //return View("PreviewPage", empobj);
+            return View("PreviewPage");
+        }
+    }
+}
