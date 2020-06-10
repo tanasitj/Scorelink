cfabf5e45eec10c472f0a3b115d7c2695a0f9db1
Marutsky <63045288+Marutsky@users.noreply.github.com>
2020-06-10 15:46:18 +0700

    updatea selectpage

--- Scorelink.web/Controllers/SelectPageController.cs
+++ Scorelink.web/Controllers/SelectPageController.cs
@@ -11,30 +11,66 @@ namespace Scorelink.web.Controllers
 {
     public class SelectPageController : Controller
     {
+        //Fix code for Test.
+        //int iUserId = 1;
+        //int iDocId = 6;
+        //----------------//
 
         DocumentInfoRepo docInfoRepo = new DocumentInfoRepo();
         DocumentDetailRepo docDetRepo = new DocumentDetailRepo();
         // GET: SelectPage
         public ActionResult Index()
         {
-
-            return View("SelectPage");
-        }
-
-        public ActionResult SelectPage(string id)
-        {
-            ViewBag.Id = id;
-            //var data = docDetRepo.Get(id);
-            //ViewBag.Id = data.DocId;
-            //ViewBag.DocDetId = data.DocDetId;
-            //ViewBag.DocPageNo = data.DocPageNo;
-            //ViewBag.PageFileName = data.PageFileName;
-            //ViewBag.PagePath = data.PagePath;
+            //    int DocId = 6;
+            //    var data = docInfoRepo.Get(DocId);
+            //    ViewBag.Id = data.DocId;
+            //    ViewBag.FilePath = data.FileUrl;
+            //    ViewBag.CreateBy = data.CreateBy;
 
             //var docInfo = docInfoRepo.Get(iDocId);
             //ViewBag.PDFPath = docInfo.FilePath;
             return View("SelectPage");
         }
 
+        public ActionResult SelectPage(int id)
+        {
+            //ViewBag.Id = id;
+            var data = docInfoRepo.Get(id);
+            ViewBag.Id = data.DocId;
+            ViewBag.FileUID = data.FileUID;
+            ViewBag.FileName = data.FileName;
+            ViewBag.FilePath = data.FilePath;
+            ViewBag.CreateBy = data.CreateBy;
+
+            //var docInfo = docInfoRepo.Get(iDocId);
+            //ViewBag.PDFPath = docInfo.FilePath;
+            return View("SelectPage");
+        }
+        //public ActionResult SelectPageNumber()
+        //{
+        //    string PageNumber = Request.Form["txtPage"];
+        //    try
+        //    {
+        //        String sCreateDate = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+        //        DocumentDetailModel doc = new DocumentDetailModel();
+        //        doc.DocId = iDocId;
+        //        doc.DocPageNo = "1";
+        //        doc.PageType = "1";
+        //        doc.CreateBy = "tanasitj";
+        //        doc.CreateDate = sCreateDate;
+
+        //        DocumentDetailRepo documentDetailRepo = new DocumentDetailRepo();
+        //        documentDetailRepo.Add(doc);
+
+        //        //return Json("OK");
+        //    }
+        //    catch (Exception ex) { };
+        //    return Json("No files selected.");
+        //}
+        public JsonResult SaveSelectPage(DocumentDetailModel item)
+        {
+            var data = docDetRepo.Add(item);
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
     }
 }
