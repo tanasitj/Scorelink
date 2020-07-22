ff9454dac5b8ae4a19d496a7767e612dc74bc14c
Marutsky <63045288+Marutsky@users.noreply.github.com>
2020-07-22 17:27:40 +0700

    up resultmodel

---
+++ Scorelink.MO/DataModel/ResultModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scorelink.MO.DataModel
+{
+    public class ResultModel
+    {
+        public List<DataResult> ScanEdit { get; set; }
+       // public string get_value { get; set; }
+    }
+    public class DataResult
+    {
+        //public string Name { get; set; }
+        //public string Address { get; set; }
+        public string Footnote_No { get; set; }
+        //public string Divisions { get; set; }
+        public SelectList Divisions { get; set; }
+        public string Digitized_Account_Title { get; set; }
+        public SelectList Recovered { get; set; }
+        //public string Recovered { get; set; }
+        public string Amount { get; set; }
+        public string Modified { get; set; }
+        public string CLCTCD { get; set; }
+        
+        
+        
+    }
+
+    public class Status
+
+    {
+        public int ID { get; set; }
+        public string StatusName { get; set; }
+    }
+}
