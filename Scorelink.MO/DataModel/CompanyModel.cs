01c2495700328cb006a4794bb3e641df5b6e5946
tanasitj <tj.tanasit@gmail.com>
2021-03-12 12:43:16 +0700

    Update Company

---
+++ Scorelink.MO/DataModel/CompanyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scorelink.MO.DataModel
+{
+    public class CompanyModel
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Domain { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
+        public string Status { get; set; }
+        public string CreateBy { get; set; }
+        public string CreateDate { get; set; }
+        public string UpdateBy { get; set; }
+        public string UpdateDate { get; set; }
+
+    }
+}
